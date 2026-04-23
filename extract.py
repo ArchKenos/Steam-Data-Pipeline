a2878e63549b46f322f986c5205a420db1d01b8e
ArchKenos <227059261+ArchKenos@users.noreply.github.com>
2026-04-23 00:20:50 +0000

    feat: add folder to keep the original data

--- extract.py
+++ extract.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import requests
+from pathlib import Path
 
 url = "https://steamspy.com/api.php?request=all&page=1"
 
@@ -7,3 +8,8 @@ response = requests.get(url)
 response.raise_for_status()
 data = response.json()
 df = pd.DataFrame(data)
+
+folder_path = Path("data") / "raw" 
+file_path = folder_path / "steamspy_data.csv"
+
+df.to_csv(file_path, index=False)
