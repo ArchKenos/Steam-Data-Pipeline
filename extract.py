0ccb556ba9d69de0b1eeb7e68ead160f718422cd
ArchKenos <227059261+ArchKenos@users.noreply.github.com>
2026-04-23 01:38:34 +0000

    feat: implement modularization of total pages respecting 1 minute delay between each one

--- extract.py
+++ extract.py
@@ -1,15 +1,30 @@
-import pandas as pd
+import json
+import time
 import requests
 from pathlib import Path
 
-url = "https://steamspy.com/api.php?request=all&page=1"
+raw_dir = Path("data") / "raw" 
+page_delay = 60 #Time to refresh page requisition
 
-response = requests.get(url)
-response.raise_for_status()
-data = response.json()
-df = pd.DataFrame(data)
+def fetch_page(page):
 
-folder_path = Path("data") / "raw" 
-file_path = folder_path / "steamspy_data.csv"
+    url = f"https://steamspy.com/api.php?request=all&page={page}"
 
-df.to_csv(file_path, index=False)
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
+
+def save_raw_data(data, page):
+    raw_dir.mkdir(parents=True, exist_ok=True)
+    file_path = raw_dir / f"steamspy_page{page}.json"
+    with open(file_path, "w", encoding="UTF-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+
+def fetch_all_games(total_pages: int = 3):
+    for page in range(0, total_pages):
+        data = fetch_page(page)
+        save_raw_data(data, page)
+        if (page < total_pages - 1):
+            time.sleep(page_delay)
+
+fetch_all_games()
