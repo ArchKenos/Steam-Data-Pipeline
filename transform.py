c6b034834d5c22096f3a7b612b5a85caf194667e
ArchKenos <227059261+ArchKenos@users.noreply.github.com>
2026-04-23 14:23:03 +0000

    feat: implement data cleaning for all raw data jsons

--- transform.py
+++ transform.py
@@ -1,20 +1,35 @@
 import pandas as pd
 from pathlib import Path
-
+import glob
 
 RAW_DIR = Path("data") / "raw" 
-raw_file = RAW_DIR / "steamspy_page0.json"
 CLEAN_DIR = Path("data") / "clean" 
-clean_file = CLEAN_DIR / 'steamspy_page0.parquet'
+data_frame = []
+def read_raw_data():
 
+    raw_pages = len(glob.glob('data/raw/*'))
 
+    for page in range (0, raw_pages):
+        raw_file = RAW_DIR / f"steamspy_page{page}.json"
 
-df = pd.read_json(raw_file, orient='index')
-df = df.reset_index(drop=True)
-df = df.drop(columns=['score_rank', 'userscore', 'average_forever', 'average_2weeks', 'median_forever', 'median_2weeks'])
+        df = pd.read_json(raw_file, orient='index')
+        df = df.reset_index(drop=True)
+        df = df.drop(columns=['score_rank', 'userscore',
+                            'average_forever', 'average_2weeks', 
+                            'median_forever', 'median_2weeks'])
+        data_frame.append(df)
+    df_save = pd.concat(data_frame)
 
+    return df_save
 
-clean_file.parent.mkdir(parents=True, exist_ok=True)
-df.to_parquet(clean_file)
+def save_clean_data(df):
+
+    clean_file = CLEAN_DIR / 'steamspy_cleaned.csv'
+    clean_file.parent.mkdir(parents=True, exist_ok=True)
+    df.to_csv(clean_file)
+
+if __name__ == '__main__':
+    df = read_raw_data()
+    save_clean_data(df)
 
 
