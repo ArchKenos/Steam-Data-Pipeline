8369abf32fd5ea8e97422d1a6a15c22c709f7a64
ArchKenos <227059261+ArchKenos@users.noreply.github.com>
2026-04-25 01:21:48 +0000

    feat: add load script into pipeline

--- pipeline.py
+++ pipeline.py
@@ -1,9 +1,11 @@
 from extract import fetch_all_games
 from transform import transform
+from load import create_table
 
 def pipeline():
-    fetch_all_games(total_pages=10)
+    fetch_all_games(total_pages=0)
     transform()
+    create_table()
 
 
 if __name__ == "__main__":
