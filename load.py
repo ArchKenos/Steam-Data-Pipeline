fd83e213bf50c361896cd5bbad00ecdc6a605ad1
ArchKenos <227059261+ArchKenos@users.noreply.github.com>
2026-04-25 01:21:06 +0000

    feat: prevent table duplicates and encapsulate logic inside function

--- load.py
+++ load.py
@@ -2,26 +2,29 @@ from pathlib import Path
 import duckdb
 
 DB_PATH = Path('data') / 'steam.db'
-
 clean_data = Path('data/clean') / 'steamspy_cleaned.parquet'
 
-with duckdb.connect (DB_PATH) as con:
-    con.execute('''CREATE TABLE IF NOT EXISTS games(
-                     appid              INT,
-                     name               VARCHAR(50),
-                     developer          VARCHAR(50),
-                     publisher          VARCHAR(50),
-                     positive           INT,
-                     negative           INT,
-                     price              INT,
-                     initialprice       INT,
-                     discount           INT,
-                     ccu                INT,
-                     owners_min         VARCHAR(15),
-                     owners_max         VARCHAR(15)
-                     )
-                     ''')
-    con.execute("INSERT INTO games" \
-    "            SELECT * " \
-    f"            FROM read_parquet('{clean_data}')")
-    con.table('games').show(max_width= 200)
+def create_table():
+    with duckdb.connect (DB_PATH) as con:
+        con.execute('''CREATE OR REPLACE TABLE games(
+                        appid              INT,
+                        name               VARCHAR(50),
+                        developer          VARCHAR(50),
+                        publisher          VARCHAR(50),
+                        positive           INT,
+                        negative           INT,
+                        price              INT,
+                        initialprice       INT,
+                        discount           INT,
+                        ccu                INT,
+                        owners_min         INT,
+                        owners_max         INT
+                        )
+                        ''')
+        con.execute("INSERT INTO games" \
+        "            SELECT * " \
+        f"            FROM read_parquet('{clean_data}')")
+        con.table('games').show(max_width= 200)
+
+if __name__ == '__main__':
+    create_table()
