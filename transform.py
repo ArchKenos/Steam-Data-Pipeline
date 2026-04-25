6fcb6c5565066946bc99950d17e0557f8f48ac17
ArchKenos <227059261+ArchKenos@users.noreply.github.com>
2026-04-25 00:46:29 +0000

    feat: split owners in two, owners_min and owners_max

--- transform.py
+++ transform.py
@@ -5,6 +5,17 @@ import glob
 RAW_DIR = Path("data") / "raw" 
 CLEAN_DIR = Path("data") / "clean" 
 
+#owners comes from API as ("10,000 .. 50,000")
+#commas must be removed and the range defined
+def  separate_owners_data(df):
+
+    df["owners"] = df["owners"].str.replace(',','')
+    owner_separation = df["owners"].str.split(" .. ",n=1, expand=True)
+    df["owners_min"] = owner_separation[0]
+    df["owners_max"] = owner_separation[1]
+    df = df.drop(columns = "owners")
+    return df
+
 def read_raw_data():
     data_frame = []
     raw_pages = len(glob.glob('data/raw/*'))
@@ -14,6 +25,7 @@ def read_raw_data():
 
         df = pd.read_json(raw_file, orient='index')
         df = df.reset_index(drop=True)
+        df = separate_owners_data(df)
         df = df.drop(columns=['score_rank', 'userscore',
                             'average_forever', 'average_2weeks', 
                             'median_forever', 'median_2weeks'])
@@ -21,6 +33,7 @@ def read_raw_data():
     df_save = pd.concat(data_frame)
 
     return df_save
+
 
 def save_clean_data(df):
 
@@ -31,7 +44,7 @@ def save_clean_data(df):
 def transform():
     df = read_raw_data()
     save_clean_data(df)
-
+    print(df.head())
 if __name__ == '__main__':
     transform()
 
